a0d4b8f9c618d4c3dbe1878b103cb5e45894a735
saika <saika@10.0.0.9>
2018-12-01 20:19:47 -0500

    intial version commit

---
+++ db-microServices/src/main/java/com/sai/repo/DbMicroServicesApplication.java
@@ -0,0 +1,12 @@
+package com.sai.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbMicroServicesApplication.class, args);
+	}
+}
